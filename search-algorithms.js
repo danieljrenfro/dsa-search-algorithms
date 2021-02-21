85609ad2d619768d5be90b1104fc3ed3448b74eb
danieljrenfro <djrenfro95@gmail.com>
2021-02-21 11:34:32 -0600

    drill 5

--- search-algorithms.js
+++ search-algorithms.js
@@ -49,16 +49,119 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(key, value) {
+    if (this.key === null) {
+      this.key = key;
+      this.value = value;
+    }
+    else if (key < this.key) {
+      if (this.left === null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    else {
+      if (this.right === null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    if (key === this.key) {
+      return this.value;
+    }
+    else if (key < this.key) {
+      return this.left.find(key);
+    }
+    else if (key > this.key) {
+      return this.right.find(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (key === this.key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key) {
+      this.left.remove(key);
+    }
+    else if (key > this.key) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.key;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right = null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
   
   //inOrder
   inOrder(values=[]) {
     if (this.left) {
-      values = this.left.dfs(values);
+      values = this.left.inOrder(values);
     }
     values.push(this.value);
 
     if (this.right) {
-      values = this.right.dfs(values);
+      values = this.right.inOrder(values);
     }
     return values;
   }
@@ -67,11 +170,11 @@ class BinarySearchTree {
   preOrder(values=[]) {
     values.push(this.value);
     if (this.left) {
-      values = this.left.dfs(values);
+      values = this.left.preOrder(values);
     }
     
     if (this.right) {
-      values = this.right.dfs(values);
+      values = this.right.preOrder(values);
     }
     return values;
   }
@@ -79,17 +182,17 @@ class BinarySearchTree {
   //postOrder
   postOrder(values=[]) {
     if (this.left) {
-      values = this.left.dfs(values);
+      values = this.left.postOrder(values);
     }
     
     if (this.right) {
-      values = this.right.dfs(values);
+      values = this.right.postOrder(values);
     }
     
     values.push(this.value);
     return values;
   }
-  
+
   // Breadth-first search (BFS)
   bfs(tree, values=[]) {
     // instantiates a new queue
@@ -121,3 +224,29 @@ class BinarySearchTree {
     return values;
   }
 }
+
+function main() {
+  const bst = new BinarySearchTree();
+
+  bst.insert(25, 25);
+  bst.insert(15, 15);
+  bst.insert(50, 50);
+  bst.insert(10, 10);
+  bst.insert(24, 24);
+  bst.insert(35, 35);
+  bst.insert(70, 70);
+  bst.insert(4, 4);
+  bst.insert(12, 12);
+  bst.insert(18, 18);
+  bst.insert(31, 31);
+  bst.insert(44, 44);
+  bst.insert(66, 66);
+  bst.insert(90, 90);
+  bst.insert(22, 22);
+
+  console.log('preOrder', bst.preOrder());
+  console.log('inOrder', bst.inOrder());
+  console.log('postOrder', bst.postOrder());
+}
+
+main();
